3578fd59d818d7b15672f81aa4b56abdc90af469
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-08-09 20:54:42 +0200

    pattern detection

--- packages/cliqz-logo-database/src/logo.js
+++ packages/cliqz-logo-database/src/logo.js
@@ -1,26 +1,31 @@
 import { getDomain, getPublicSuffix } from 'tldjs';
 import defaultDatabase from './logo-database.json';
 
-const findRule = (database, name) => {
-  const rules = database.domains[name];
-  return rules[0];
-};
-
 export default function (url, { database = defaultDatabase } = {}) {
-  const publicSuffix = getPublicSuffix(url);
-  const domain = getDomain(url);
-  const base = domain.substring(0, domain.length - publicSuffix.length - 1);
-  const rule = findRule(database, base);
-
   /* eslint-disable */
   const version = database.__version;
   /* eslint-enable */
 
-  const logoUrl = `https://cdn.cliqz.com/brands-database/database/${version}/logos/${base}/${rule.r}.svg`;
-  const color = rule.b;
+  const publicSuffix = getPublicSuffix(url);
+  const hostname = getDomain(url);
+
+  const base = hostname.substring(0, hostname.length - publicSuffix.length - 1);
+  const baseIndex = hostname.lastIndexOf(base);
+  const prefix = hostname.substr(0, baseIndex);
+  const postfix = hostname.substr(baseIndex + base.length);
+  const pattern = `${prefix}$${postfix}`;
+
+  const rules = database.domains[base];
+  let rule = rules.find(({ r }) => r === pattern);
+
+  if (rules.length === 1) {
+    rule = rules[0];
+  } else if (!rule) {
+    rule = rules[rules.length - 1];
+  }
 
   return {
-    logoUrl,
-    color,
+    logoUrl: `https://cdn.cliqz.com/brands-database/database/${version}/logos/${base}/${rule.r}.svg`,
+    color: rule.b,
   };
 }
--- packages/cliqz-logo-database/tests/node/logo-test.js
+++ packages/cliqz-logo-database/tests/node/logo-test.js
@@ -2,10 +2,28 @@ import { expect } from 'chai';
 import getLogo from '../logo';
 
 const fakeDatabase = {
+  __version: '124',
   domains: {
     'cliqz': [{
       r: '$',
       b: '111111',
+    }],
+    'mozilla': [{
+      r: '',
+      b: '111111',
+    }, {
+      r: '',
+      b: '222222',
+    }],
+    'eff': [{
+      r: '$',
+      b: '111111',
+    }, {
+      r: '$.org',
+      b: '222222',
+    }, {
+      r: '$.com',
+      b: '333333',
     }],
   },
 };
@@ -30,6 +48,34 @@ describe('getLogo', function () {
       expect(subject)
         .to.have.property('logoUrl')
         .that.equal(`https://cdn.cliqz.com/brands-database/database/${fakeDatabase.__version}/logos/cliqz/$.svg`);
+    });
+  });
+
+  context('with multiple patterns', function () {
+    beforeEach(function () {
+      subject = getLogo('https://eff.org', {
+        database: fakeDatabase,
+      });
+    });
+
+    it('uses matching rule', function () {
+      expect(subject)
+        .to.have.property('color')
+        .that.equal(fakeDatabase.domains.eff[1].b);
+    });
+  });
+
+  context('with non matching pattern', function () {
+    beforeEach(function () {
+      subject = getLogo('https://mozilla.com', {
+        database: fakeDatabase,
+      });
+    });
+
+    it('uses last rule', function () {
+      expect(subject)
+        .to.have.property('color')
+        .that.equal(fakeDatabase.domains.mozilla[1].b);
     });
   });
 });
