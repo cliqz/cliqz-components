7d69c71441bf1d320566d7ab96f48758efb1d53c
Khaled Tantawy <khaled@cliqz.com>
2019-05-02 12:43:06 +0200

    Implement news snippet

---
+++ packages/cliqz-component-ui-snippet-news/stories/news-result.ts
@@ -0,0 +1,60 @@
+export const NewsResult = {
+  links: [
+    {
+      extra: {
+        breaking: false,
+        creation_timestamp: 1534227823,
+        description:
+          'Frust bei Mesut Özil (29) und dem FC Arsenal! Bei der Auftaktpleite gegen Manchester City (0:2) war die Mannschaft des neuen Trainers Unai Emery chancenlos,',
+        domain: 'bild.de',
+        media:
+          'https://imgr.cliqz.com/iZ42Dz5hwECjfv91LoUiCv55upC9wvwKiQZx4h1ZyPg/fill/0/200/no/1/aHR0cHM6Ly9iaWxkZXIuYmlsZC5kZS9mb3Rvcy9tZXN1dC1vZXppbC1oYXJ0ZS1rcml0aWstbmFjaC1hcnNlbmFsLXBsZWl0ZS1nZWdlbi1tYW5jaGVzdGVyLWNpdHktMjAwODg1NjU4LTU2NjYxMDY0L0JpbGQvMS5iaWxkLmpwZw.jpg',
+        murl:
+          'https://m.bild.de/sport/fussball/mesut-oezil/mesut-oezil-harte-kritik-nach-arsenal-pleite-gegen-manchester-city-56661036.bildMobile.html',
+        thumbnail:
+          'https://imgr.cliqz.com/iZ42Dz5hwECjfv91LoUiCv55upC9wvwKiQZx4h1ZyPg/fill/0/200/no/1/aHR0cHM6Ly9iaWxkZXIuYmlsZC5kZS9mb3Rvcy9tZXN1dC1vZXppbC1oYXJ0ZS1rcml0aWstbmFjaC1hcnNlbmFsLXBsZWl0ZS1nZWdlbi1tYW5jaGVzdGVyLWNpdHktMjAwODg1NjU4LTU2NjYxMDY0L0JpbGQvMS5iaWxkLmpwZw.jpg',
+      },
+      title:
+        'Mesut Özil: Harte Kritik nach Arsenal-Pleite gegen Manchester City',
+      url:
+        'https://www.bild.de/sport/fussball/mesut-oezil/mesut-oezil-harte-kritik-nach-arsenal-pleite-gegen-manchester-city-56661036.bild.html',
+    },
+    {
+      extra: {
+        breaking: false,
+        creation_timestamp: 1534231863,
+        description:
+          'Loverboy Alparslan G. schickte mehr als ein Jahr lang ein Mädchen (14) auf den Strich. Jetzt wurde er verurteilt.',
+        domain: 'bild.de',
+        media:
+          'https://imgr.cliqz.com/jV17BNsCj7eI8R1rQ0oCbUlpzAqfbZbiKanAAfJo7L4/fill/0/200/no/1/aHR0cHM6Ly9iaWxkZXIuYmlsZC5kZS9mb3Rvcy9hbHBhcnNsYW4tZy0yNC13dXJkZS16dS16d2VpLWphaHJlbi1hdWYtYmV3YWVocnVuZy12ZXJ1cnRlaWx0LTIwMDg4NTcxMy01NjY2MTg1NC9CaWxkLzIuYmlsZC5qcGc.jpg',
+        murl:
+          'https://m.bild.de/regional/duesseldorf/prostituierte/mann-schickt-maedchen-auf-den-strich-56661682.bildMobile.html',
+        thumbnail:
+          'https://imgr.cliqz.com/jV17BNsCj7eI8R1rQ0oCbUlpzAqfbZbiKanAAfJo7L4/fill/0/200/no/1/aHR0cHM6Ly9iaWxkZXIuYmlsZC5kZS9mb3Rvcy9hbHBhcnNsYW4tZy0yNC13dXJkZS16dS16d2VpLWphaHJlbi1hdWYtYmV3YWVocnVuZy12ZXJ1cnRlaWx0LTIwMDg4NTcxMy01NjY2MTg1NC9CaWxkLzIuYmlsZC5qcGc.jpg',
+      },
+      title: 'Bewährungsstrafe - Loverboy schickt Mädchen (14) auf den Strich',
+      url:
+        'https://www.bild.de/regional/duesseldorf/prostituierte/mann-schickt-maedchen-auf-den-strich-56661682.bild.html',
+    },
+    {
+      extra: {
+        breaking: false,
+        creation_timestamp: 1534226054,
+        description:
+          'Magdeburg – Blutige Auseinandersetzung am Magdeburger Hasselbachplatz. Schon wieder gingen am beliebten City-Treffpunkt mehrere Männer aufeinander los. Dabei wurde ein Beteiligter mit dem Messer',
+        domain: 'bild.de',
+        media:
+          'https://imgr.cliqz.com/n1hWNN8CXKZ8naj8hCcic4sXaqoB38sb_VikrwsgLp8/fill/0/200/no/1/aHR0cHM6Ly9iaWxkZXIuYmlsZC5kZS9mb3Rvcy9wb2xpemVpLXVuZC1yZXR0dW5nc2RpZW5zdC13YXJlbi1taXQtZWluZW0tZ3Jvc3NhdWZnZWJvdC1hbS1oYXNzZWxiYWNocGxhdHotaW4tbWFnZGVidXJnLWVpbmdlcnVlY2t0LTIwMDg4NTYzMC01NjY2MDc3NC9CaWxkLzIuYmlsZC5qcGc.jpg',
+        murl:
+          'https://m.bild.de/regional/sachsen-anhalt/schlaegerei/mann-in-magdeburg-niedergestochen-56660700.bildMobile.html',
+        thumbnail:
+          'https://imgr.cliqz.com/n1hWNN8CXKZ8naj8hCcic4sXaqoB38sb_VikrwsgLp8/fill/0/200/no/1/aHR0cHM6Ly9iaWxkZXIuYmlsZC5kZS9mb3Rvcy9wb2xpemVpLXVuZC1yZXR0dW5nc2RpZW5zdC13YXJlbi1taXQtZWluZW0tZ3Jvc3NhdWZnZWJvdC1hbS1oYXNzZWxiYWNocGxhdHotaW4tbWFnZGVidXJnLWVpbmdlcnVlY2t0LTIwMDg4NTYzMC01NjY2MDc3NC9CaWxkLzIuYmlsZC5qcGc.jpg',
+      },
+      title: 'Bei Prügelorgie in Magdeburg - Mann in der City niedergestochen!',
+      url:
+        'https://www.bild.de/regional/sachsen-anhalt/schlaegerei/mann-in-magdeburg-niedergestochen-56660700.bild.html',
+    },
+  ],
+  type: 'news',
+};
