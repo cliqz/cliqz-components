b8f214eb6f9cced440ba01318f24058cf445cb29
Rémi Berson <remi@cliqz.com>
2019-07-30 17:03:24 +0200

    add metalint to project

--- packages/cliqz-logo-database/src/index.ts
+++ packages/cliqz-logo-database/src/index.ts
@@ -1,3 +1,11 @@
+/*!
+ * Copyright (c) 2019-present Cliqz GmbH. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import getLogo from './logo';
 import defaultDatabase from './logo-database';
 
