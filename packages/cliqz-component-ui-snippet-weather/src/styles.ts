68ed50e6363f9fcdd450f598e4161a41937d2aca
Krzysztof Jan Modras <chrmod@chrmod.net>
2019-11-26 15:14:15 +0100

    fixing linter errors

--- packages/cliqz-component-ui-snippet-weather/src/styles.ts
+++ packages/cliqz-component-ui-snippet-weather/src/styles.ts
@@ -1,17 +1,17 @@
 import { ViewStyle } from 'react-native';
 
-type WebStyles = {
+interface WebStyles {
   cursor?: 'pointer' | 'default';
-};
+}
 
 export type CliqzViewStyle = ViewStyle & WebStyles;
 
-type UpdateSpec<T> = {[P in keyof T]?: T[P] & UpdateSpec<T[P]>};
+type UpdateSpec<T> = { [P in keyof T]?: T[P] & UpdateSpec<T[P]> };
 
 export function getStyle<T>(base: T, partial: Partial<UpdateSpec<T>> = {}): T {
-  const result: T = {...base};
+  const result: T = { ...base };
 
-  for (const key of (Object.keys(partial) as (Array<keyof T>))) {
+  for (const key of Object.keys(partial) as (Array<keyof T>)) {
     const value: T[keyof T] | undefined = partial[key];
     if (value !== undefined) {
       result[key] = { ...result[key], ...value };
@@ -21,7 +21,10 @@ export function getStyle<T>(base: T, partial: Partial<UpdateSpec<T>> = {}): T {
   return result;
 }
 
-export function pickStyle<T, K extends keyof T>(fullStyles: Partial<T>, properties: K[]): Partial<T> {
+export function pickStyle<T, K extends keyof T>(
+  fullStyles: Partial<T>,
+  properties: K[],
+): Partial<T> {
   const pickedStyles: Partial<T> = {};
   properties.forEach((property) => {
     if (fullStyles[property]) {
