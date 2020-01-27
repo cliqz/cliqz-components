6b97683823814c13f5428b1589ac42058cc6c6be
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-01-27 13:23:24 +0100

    another rewrite

--- packages/component-ui-result/src/index.tsx
+++ packages/component-ui-result/src/index.tsx
@@ -1,67 +1,127 @@
-import React, { FunctionComponent, ClassType } from 'react';
-import { Image, Text, View } from 'react-native';
+import React, { useContext, useReducer, useMemo } from 'react';
 
-export interface ResultData {
-  title: string;
-  subResults?: ResultData[];
+type Dispatch = (action: Action) => void
+
+type State = {
+  results: any[];
+  index: number;
 }
 
-export interface ResultComponentProps {
-  result: ResultData;
-  selectedIndex: number;
-  registerResult(): number;
+enum ActionType {
+  next = "next",
+  previous = "previous",
+  register = "register",
 }
 
-export interface ResultComponent {
-  next(): boolean;
-  previous(): boolean;
-}
+type Action =
+  {
+    type: ActionType.next
+  } |
+  {
+    type: ActionType.previous
+  } |
+  {
+    type: ActionType.register
+    result: any
+  };
 
-interface ResultProps {
-}
+const SelectableResultStateContext = React.createContext<State>({
+  index: -1,
+  results: [],
+});
+const SelectableResultDispatchContext = React.createContext<Dispatch | undefined>(undefined);
 
-export class Result extends React.Component<ResultProps> {
-  resultCount = -1
+const exhaustiveCheck = (_: never): void => {};
 
-  state = {
-    selectedResultIndex: -1,
-  }
-
-  resultsRefs: React.RefObject<ResultComponent>[] = []
-  registerResult = (): number => {
-    this.resultCount += 1
-    return this.resultCount;
-  }
-
-  next() {
-    if (this.state.selectedResultIndex >= this.resultCount) {
-      this.setState({
-        selectedResultIndex: 0,
-      });
-      return;
+function selectableResultReducer(state: State, action: Action): State {
+  switch (action.type) {
+    case ActionType.register: {
+      return {
+        ...state,
+        results: [
+          ...state.results,
+          action.result,
+        ],
+      };
     }
-    this.setState({
-      selectedResultIndex: this.state.selectedResultIndex + 1,
-    })
+    case ActionType.next:
+      if (state.index >= state.results.length - 1) {
+        return {
+          ...state,
+          index: -1,
+        };
+      }
+      return {
+        ...state,
+        index: state.index + 1,
+      };
+    case ActionType.previous:
+      if (state.index === - 1) {
+        return {
+          ...state,
+          index: state.results.length - 1,
+        };
+      }
+      return {
+        ...state,
+        index: state.index - 1,
+      };
+    default:
+      exhaustiveCheck(action);
+      return state;
+  }
+}
+
+type ResultListControls = {
+  selectedResultIndex: number
+  next(): void
+  previous(): void
+}
+
+export const ResultList = ({ children }: { children(arg0: ResultListControls): any }) => {
+  const [state, dispatch] = useReducer(selectableResultReducer, {
+    index: -1,
+    results: [],
+  });
+
+  return (
+    <SelectableResultStateContext.Provider value={state}>
+      <SelectableResultDispatchContext.Provider value={dispatch}>
+        {children({
+          selectedResultIndex: state.index,
+          next() {
+            dispatch({ type: ActionType.next });
+          },
+          previous() {
+            dispatch({ type: ActionType.previous });
+          },
+        })}
+      </SelectableResultDispatchContext.Provider>
+    </SelectableResultStateContext.Provider>
+  );
+}
+
+type SelectableResultContext = {
+  isActive: boolean,
+  index: number,
+}
+
+export const SelectableResult = ({ children }: { children(arg0: SelectableResultContext): any }) => {
+  const { index, results } = useContext(SelectableResultStateContext);
+  const dispatch = useContext(SelectableResultDispatchContext)
+  if (!dispatch) {
+    throw new Error('SelectableResult has to be nested in ResultList');
   }
 
-  previous() {
-    if (this.state.selectedResultIndex === 0) {
-      this.setState({
-        selectedResultIndex: this.resultCount,
-      });
-      return;
-    }
-    this.setState({
-      selectedResultIndex: this.state.selectedResultIndex - 1,
-    })
-  }
+  const id = useMemo(() => {
+    const id = {};
+    dispatch && dispatch({ type: ActionType.register, result: id });
+    return id;
+  }, []);
+  const indexOfId = results.indexOf(id);
 
-  render() {
-    return (
-      <div>
-        {typeof this.props.children === "function" ? this.props.children(this) : null}
-      </div>
-    );
-  }
+  return children({
+    isActive: index === indexOfId,
+    index: indexOfId,
+  });
 }
--- packages/component-ui-result/stories/index.story.tsx
+++ packages/component-ui-result/stories/index.story.tsx
@@ -1,104 +1,56 @@
 import { storiesOf } from '@storybook/react';
-import React, { useMemo } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import { button } from "@storybook/addon-knobs";
-import { Result, ResultData, ResultComponentProps, ResultComponent } from '../src/index';
+import { ResultList, SelectableResult } from '../src/index';
 
-const SimpleResult = ({ result, registerResult, selectedIndex }: { result: any, registerResult: any, selectedIndex: any }) => {
-  const index = useMemo(registerResult, []);
+const TitleResult = ({ title, children }: { title: string, children?: any }) => {
   return (
-    <View style={{ backgroundColor: selectedIndex === index ? 'green' : 'transparent' }}>
-      <Text>
-        {'simple: ' + result.title + ' ' + index + ' ' + selectedIndex}
-      </Text>
+    <View>
+       <SelectableResult>
+        {({ isActive, index }) =>
+          <View style={{ backgroundColor: isActive ? 'green' : 'transparent' }}>
+            <Text>{index}: {title}</Text>
+          </View>
+        }
+      </SelectableResult>
+      {children}
     </View>
-  )
-}
-
-class GenericResult extends React.Component<ResultComponentProps> {
-  state = {
-    isActive: false,
-  }
-  index1: number
-  index2: number
-
-  constructor(props: ResultComponentProps) {
-    super(props);
-    this.index1 = props.registerResult();
-    this.index2 = props.registerResult();
-  }
-
-  render() {
-    return (
-      <>
-        <View style={{ backgroundColor: this.props.selectedIndex === this.index1 ? 'green' : 'transparent' }}>
-          <Text>
-            {'1: ' + this.props.result.title + ' ' + this.index1 + ' ' + this.props.selectedIndex}
-          </Text>
-        </View>
-        <View style={{ backgroundColor: this.props.selectedIndex === this.index2 ? 'green' : 'transparent' }}>
-          <Text>
-            {'2: ' + this.props.result.title + ' ' + this.index2 + ' ' + this.props.selectedIndex}
-          </Text>
-        </View>
-        <SimpleResult result={{ title: 'test' }} registerResult={this.props.registerResult} selectedIndex={this.props.selectedIndex} />
-      </>
-    )
-  }
-}
-
-const mapResultToComponent = (result: ResultData, selectableResultManager: any) =>
-  (result.subResults && result.subResults.length > 0)
-    ? <Result
-      key={result.title}
-    >
-      {result.subResults.map(mapResultToComponent)}
-    </Result>
-    : <GenericResult
-      selectedIndex={selectableResultManager.state.selectedResultIndex}
-      result={result}
-      key={result.title}
-      registerResult={selectableResultManager.registerResult}
-    />;
-
-const resultRef: React.RefObject<Result> = React.createRef();
-
-button('Keyboard: up', () => {
-  const ref = resultRef.current;
-  if (ref) {
-    ref.previous();
-  }
-});
-
-button('Keyboard: down', () => {
-  const ref = resultRef.current;
-  if (ref) {
-    ref.next();
-  }
-});
-
-const results = [
-  {
-    title: "hello"
-  },
-  {
-    title: "world"
-  },
-  {
-    title: "today"
-  }
-];
+  );
+};
 
 storiesOf('Result', module).add('Results', () => {
+  let nextAction: CallableFunction | undefined
+  let previousAction: CallableFunction | undefined;
+
+  button('Keyboard: up', () => {
+    if (!previousAction) { return }
+    previousAction();
+  });
+
+  button('Keyboard: down', () => {
+    if (!nextAction) { return }
+    nextAction();
+  });
+
   return (
-    <div>
-      <Result
-        ref={resultRef}
-      >
-        {(selectableResultManager: any) =>
-          results.map(r => mapResultToComponent(r, selectableResultManager))
-        }
-      </Result>
-    </div>
-  )
+    <View>
+      <ResultList>
+        {({ next, previous, selectedResultIndex }) => {
+          nextAction = next;
+          previousAction = previous;
+          return (
+            <>
+              <Text>Currently selected result index: {selectedResultIndex}</Text>
+              <TitleResult title="Hello">
+                <TitleResult title="sub 1" />
+                <TitleResult title="sub 2" />
+              </TitleResult>
+              <TitleResult title="World" />
+            </>
+          );
+        }}
+      </ResultList>
+    </View>
+  );
 });
